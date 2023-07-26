98b9cb9debf40b2fc9f57dc52d24f62f9e4889a0
xbreizh <xavier.lamourec@gmail.com>
2023-07-26 18:08:36 +0200

    hide left bar and shuffle on film details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'imdb-api';
-  
+export class AppComponent implements OnInit {
+  showLeftBar: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Listen to router events
+    this.router.events
+      .pipe(filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // Check if the current route is the film detail page
+        this.showLeftBar = !event.url.includes('/film/');
+      });
+  }
 }
