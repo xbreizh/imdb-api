ebc1651ddd73220b547ba0ba0ed7940204bb8825
xbreizh <xavier.lamourec@gmail.com>
2023-08-10 17:39:56 +0200

    reset film list when clicking on logo

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FilmService } from '../film.service';
 
 @Component({
   selector: 'app-logo',
@@ -9,12 +10,14 @@ import { Router } from '@angular/router';
 export class LogoComponent {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private filmService: FilmService
     ) {
 
   }
 
   goBackToList(): void {
     this.router.navigate(['/']);
+    this.filmService.resetSearchCriteria();
   }
 }
